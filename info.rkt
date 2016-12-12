8f6fc39fcf1ef06ee08e1e5c87464a33f46144c9
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-13 00:47:23 +0800

    Update Mission: info.rkt: define docs

--- info.rkt
+++ info.rkt
@@ -7,3 +7,5 @@
 (define version "1.0")
 (define deps '("base" "digimon" "db-lib" "typed-racket-lib" "typed-racket-more"))
 (define build-deps '("scribble-lib" "racket-doc"))
+
+(define scribblings '(["tamer/schema.scrbl" (main-doc) ("Databases")]))
