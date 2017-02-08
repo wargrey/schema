252f2fa2a9816c2d08e8a485298cd546770f7a71
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-02-09 05:37:43 +0800

    fix the sign problem of 64bit primary key in sqlite3

--- uupk.rkt
+++ uupk.rkt
@@ -1,6 +1,6 @@
 #lang typed/racket/base
 
-(provide (except-out (all-defined-out) variant+clock-sequence))
+(provide (all-defined-out))
 (provide (all-from-out typed/racket/random))
 
 (require racket/fixnum)
@@ -14,28 +14,31 @@
   (lambda [year [month 1] [day 1] [hour 0] [minute 0] [second 0] [local? #true]]
     (find-seconds second minute hour day month year local?)))
 
+;;; WARNING
+; The actual generated 64bit primary keys only have 63 significant bits,
+;  since integers in SQLite3 are signed and the overflowed ones will be converted to `real`s unrecognized,
+;  nonetheless, 63bit is still reasonably good enough.
+
 (define pk64:timestamp : (->* () (Integer) Integer)
   (lambda [[diff:s 0]]
-    (define version : Byte 1) #b11
+    (define version : Byte #b01)
     (define now:ms : Flonum (current-inexact-milliseconds))
     (define now:s : Integer (fxquotient (exact-floor now:ms) 1000))
-    (define diff32:s : Fixnum (fxand (fx- now:s diff:s) #xFFFF))
-    (define us : Fixnum (fx- (exact-floor (fl* now:ms 1000.0)) (fx* now:s 1000000)))
-    (bitwise-ior (arithmetic-shift diff32:s 32)
+    (define ts32 : Fixnum (fxand (fx- now:s diff:s) #x7FFF))
+    (define us16 : Fixnum (fx- (exact-floor (fl* now:ms 1000.0)) (fx* now:s 1000000)))
+    (define random-clock14 : Integer (fxand (current-memory-use) #b11111111111111))
+    (bitwise-ior (arithmetic-shift ts32 32)
                  (arithmetic-shift version 30)
-                 (fxlshift us 14)
-                 (variant+clock-sequence))))
+                 (fxlshift us16 14)
+                 random-clock14)))
 
-(define pk64:random : (->* () (Integer) Integer)
-  (lambda [[diff:s 0]]
-    (define version : Byte 2) #b11
-    (define diff32:s : Fixnum (fxand (fx- (current-seconds) diff:s) #xFFFF))
-    (define urnd : Integer (integer-bytes->integer (crypto-random-bytes 2) #false #true))
-    (bitwise-ior (arithmetic-shift diff32:s 32)
+(define pk64:random : (->* () () Integer)
+  (lambda []
+    (define version : Byte #b10)
+    (define ts32 : Integer (bitwise-and (integer-bytes->integer (crypto-random-bytes 4) #false #true) #x7FFF))
+    (define urnd16 : Integer (integer-bytes->integer (crypto-random-bytes 2) #false #true))
+    (define clock14 : Integer (random #b11111111111111))
+    (bitwise-ior (arithmetic-shift ts32 32)
                  (arithmetic-shift version 30)
-                 (fxlshift (variant+clock-sequence) 16)
-                 (fxand urnd #xFFFF))))
-
-(define variant+clock-sequence : (-> Index)
-  (lambda [] ; TODO: what if the clock is set backwards?
-    (fxand (current-memory-use) #b11111111111111)))
+                 (fxlshift urnd16 14)
+                 clock14)))
